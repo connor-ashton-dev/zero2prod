cf2dde9ccb5523ef901891171f6b929efc87728d
Connor Ashton <cjconnorashton@gmail.com>
2024-05-05 14:50:24 -0600

    testing CI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read config");
+    let addr = format!("127.0.0.1:{}", configuration.application_port);
+
+    let tcp_listener = TcpListener::bind(addr)?;
+    run(tcp_listener)?.await
 }
