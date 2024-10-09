151724b966fa15d47384795a078cd47273018011
Connor Ashton <cjconnorashton@gmail.com>
2024-10-08 18:12:05 -0600

    telemetry, testing, postgres, refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::net::TcpListener;
+// STOPPING AT 4.5.11
 
+use sqlx::PgPool;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read config");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+
+    run(listener, connection_pool)?.await
 }
